c6d2cdbdaa11065d9bf61c61b5263f8fb7f01d1d
Clémence Métayer <metayer.clemence@yahoo.com>
2023-12-05 11:51:11 +0100

    Ajout du subplot

--- Interpolation_processus_gaussiens.py
+++ Interpolation_processus_gaussiens.py
@@ -107,10 +107,12 @@ def plot_GP(X_observation, y_observation, mean_per, t, var_per, name_specie):
         fig, axes = plt.subplots(1,1)
         axes.scatter(X_observation, y_observation, marker='x', color='black') # data
         
-        axes.plot(t, mean_per, color='darkblue', linewidth=2)
-        axes.fill_between(t, mean_per - 2*torch.sqrt(var_per), mean_per + 2*torch.sqrt(var_per),
-                           alpha=.2, color='darkblue')
+        axes.plot(t, mean_per.detach().numpy(), color='darkblue', linewidth=2)  # Detach the tensor
+        axes.fill_between(t, (mean_per - 2 * torch.sqrt(var_per)).detach().numpy(), 
+                          (mean_per + 2 * torch.sqrt(var_per)).detach().numpy(),
+                          alpha=.2, color='darkblue')
         axes.set_xlabel('Time $t$ (hours)')
+        axes.set_ylabel('Concentration (nmol/L)')
         axes.set_title(str(name_specie))
         fig.tight_layout()
         fig.savefig("results/"+str(name_specie)+".png", dpi=300)
@@ -127,7 +129,9 @@ X_observation = rna_seq_data["CTs"]*3
 kernel_method = "Range"
 package_method = "Pytorch"
 
-for name in list_species : 
+res_rna_seq = {name : {"X_observation_tensor" :{}, "y_observation_tensor_normalized" :{}, "time" :{}, "mean_specie":{}, "var_specie":{}} for name in list_species}
+
+for i, name in enumerate(list_species) : 
     y_observation = np.concatenate((rna_seq_data["ctrl"][name]["pts"][:,0], rna_seq_data["ctrl"][name]["pts"][:,1], rna_seq_data["ctrl"][name]["pts"][:,2]), axis=0)
 
     # Missing data handling
@@ -148,4 +152,46 @@ for name in list_species :
     
     mean_specie, var_specie, time = interpolation_GP_arn(X_observation_tensor, y_observation_tensor_normalized, type_kernel = kernel_method, type_package = package_method, name_specie = name)
     plot_GP(X_observation_tensor, y_observation_tensor_normalized, mean_specie, time, var_specie, name)
+
+    res_rna_seq[name]["X_observation_tensor"] = X_observation_tensor
+    res_rna_seq[name]["y_observation_tensor_normalized"] = y_observation_tensor_normalized
+    res_rna_seq[name]["time"] = time
+    res_rna_seq[name]["mean_specie"] = mean_specie
+    res_rna_seq[name]["var_specie"] = var_specie
+    
+
+# Calcul du nombre de lignes nécessaire pour afficher tous les graphiques
+num_rows = 2  # Nombre de lignes souhaité
+num_cols = -(-len(list_species) // num_rows)  # Calcul du nombre de colonnes nécessaire
+
+# Ajout du subplot à partir du dictionnaire res_rna_seq
+fig, axes = plt.subplots(num_rows, num_cols, figsize=(15, 5*num_rows))
+
+for i, name in enumerate(list_species):
+    row, col = i // num_cols, i % num_cols  # Calcul de la position du subplot dans la grille
+    X_observation_tensor = res_rna_seq[name]["X_observation_tensor"]
+    y_observation_tensor_normalized = res_rna_seq[name]["y_observation_tensor_normalized"]
+    mean_specie = res_rna_seq[name]["mean_specie"]
+    time = res_rna_seq[name]["time"]
+    var_specie = res_rna_seq[name]["var_specie"]
+
+    axes[row, col].scatter(X_observation_tensor, y_observation_tensor_normalized, marker='x', color='black')  # data
+
+    axes[row, col].plot(time, mean_specie.detach().numpy(), color='darkblue', linewidth=2)  # Detach the tensor
+    axes[row, col].fill_between(time, (mean_specie - 2 * torch.sqrt(var_specie)).detach().numpy(),
+                                (mean_specie + 2 * torch.sqrt(var_specie)).detach().numpy(),
+                                alpha=.2, color='darkblue')
+    axes[row, col].set_xlabel('Time $t$ (hours)')
+    axes[row, col].set_ylabel('Concentration (nmol/L)')
+    axes[row, col].set_title(str(name))
+
+# Supprimer les sous-graphiques inutilisés
+for i in range(len(list_species), num_rows * num_cols):
+    fig.delaxes(axes.flatten()[i])
+
+fig.tight_layout()
+plt.savefig("results/GP_RNA_Seq.png", dpi=300)
+plt.show()
+    
+    
     
